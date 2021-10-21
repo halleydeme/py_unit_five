2a9f6c222ca61c5136816d59cc0ed060d8cebde8
halley <demeha@ssfs.org>
2021-10-21 11:55:57 -0400

    multiplication for loops

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,16 @@
 def fibonacci(x):
-    """
-    Ex. fibonacci(5) returns "1 1 2 3 5 "
-    :param number: The number of Fibonacci terms to return
-    :return: A string consisting of a number of terms of the Fibonacci sequence.
-    """
+
+    a = 0
+    b = 1
+    test = "0 1 "
+    for p in range(x):
+        c = a + b
+        a = b
+        b = c
+        test += str(c) + " "
+    return test
+
+def main():
+    print(fibonacci(9))
+
+main()
--- multiplication.py
+++ multiplication.py
@@ -1,1 +1,9 @@
 def multiplication_table(number):
+    test = ""
+    for x in range(1,13):
+        test += str(number*x ) + " "
+    return test
+def main():
+   print(multiplication_table(5))
+
+main()
